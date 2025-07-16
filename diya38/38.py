a651bc1a4ce1c5896d8a861830a1e510b1fcbe69
Diyapatel311 <diyapatel1128@gmail.com>
2025-07-16 15:45:58 +0530

    Add files via upload

---
+++ diya38/38.py
@@ -0,0 +1,20 @@
+print ("Hello python")
+n = 7
+print(n ** 2)
+colors = ["red", "blue", " yellow"]
+print(colors)
+x = 10
+print(x)
+y = 3.14
+print(y)
+name = "Diya"
+print(name)
+bool = True
+print(bool)
+coordinates = (10, 20)
+print(coordinates)
+numbers = {1, 2, 3}
+print(numbers)
+person = {"name": "John", "age": 30}
+print(person)
+
---
+++ diya38/Untitled-1.py
@@ -0,0 +1,5 @@
+print ("Hello python")
+n = 7
+print(n ** 2)
+colors = ["red", "blue", " yellow"]
+print(colors)
